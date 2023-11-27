f4816acb8ece77a326d9166ef0d9f2c81d000d35
Trialolalo <nuriabonelquesada@gmail.com>
2023-11-27 14:13:51 +0100

    Sass-y-maquetación

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -35,25 +35,16 @@
        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
       <!-- Enlazamos con la página de CSS -->
-      <link rel="stylesheet" href="/style/style.css">
+      @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
       <!-- Enlazamos con la página de JS -->
-      <script type="module" src="js/app.js"></script>
+      
     </head>
 
 
     <body>
-        <header>
 
-            <div class="logo">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13 2V3H12V9H11V10H9V11H8V12H7V13H5V12H4V11H3V9H2V15H3V16H4V17H5V18H6V22H8V21H7V20H8V19H9V18H10V19H11V22H13V21H12V17H13V16H14V15H15V12H16V13H17V11H15V9H20V8H17V7H22V3H21V2M14 3H15V4H14Z" /></svg>
-            </div>
-
-            <div class="burger-menu">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg>
-            </div>
-
-        </header>
+        @include('admin.layout.partials.header')
 
         <main>
             @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', function () {
     return view('welcome', ['name' => 'Nuria']);
 });
 
-Route::get('/admin/users', function () {
+Route::get('/admin/usuarios', function () {
     return view('admin.users.index');
 });
 
-Route::get('/admin/events', function () {
+Route::get('/admin/eventos', function () {
     return view('admin.events.index');
 });
