b7abda90a83d765a397bf6f1f34fcf15041fa5ff
Trialolalo <nuriabonelquesada@gmail.com>
2023-11-27 15:52:10 +0100

    JS-update

--- Trabajillo/filter.js
+++ Trabajillo/filter.js
@@ -1,14 +1,10 @@
 export default (() => {
 
-    const filter = document.querySelector(".filter")
     const filterSlide = document.querySelector(".filter-slide");
-    const filterMenu = document.querySelector(".filter-menu");
-    const filterForm = document.querySelector(".filter-form")
+    const filterModal = document.querySelector(".filter-modal");
   
     filterSlide.addEventListener("click", () => {
-        filterSlide.classList.toggle("active");
-        filterMenu.classList.toggle("active");
-        filterForm.classList.toggle("active");
+        filterModal.classList.toggle("active");
     });
 
 })();
